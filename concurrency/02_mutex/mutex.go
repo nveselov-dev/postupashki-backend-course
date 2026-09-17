9e7dc9bfbd7b2f2a19c613d94b7eaa5f58ad390e
Nikolai Veselov <143344092+DirtyDevel0per@users.noreply.github.com>
2026-09-17 22:08:12 +0300

    add mutex

--- concurrency/02_mutex/mutex.go
+++ concurrency/02_mutex/mutex.go
@@ -1,5 +1,11 @@
 package mutex
 
+import (
+	"primitives/internal/futex"
+	"runtime"
+	"sync/atomic"
+)
+
 const (
 	free = iota
 	held
@@ -11,13 +17,39 @@ type Mutex struct {
 }
 
 func (m *Mutex) Lock() {
-	panic("не реализовано")
+	if m.TryLock() {
+		return
+	}
+
+	for {
+		if atomic.LoadUint32(&m.state) == contended || atomic.CompareAndSwapUint32(&m.state, held, contended) {
+
+			for range 100 {
+				if atomic.LoadUint32(&m.state) == free {
+					break
+				}
+				runtime.Gosched()
+			}
+
+			futex.Wait(&m.state, contended)
+		}
+		runtime.Gosched()
+		if atomic.CompareAndSwapUint32(&m.state, free, contended) {
+			return
+		}
+	}
 }
 
 func (m *Mutex) TryLock() bool {
-	panic("не реализовано")
+	return atomic.CompareAndSwapUint32(&m.state, free, held)
 }
 
 func (m *Mutex) Unlock() {
-	panic("не реализовано")
+	old := atomic.SwapUint32(&m.state, free)
+	if old == free {
+		panic("unlock of unlocked lock")
+	}
+	if old == contended {
+		futex.Wake(&m.state)
+	}
 }
